12e1a8f29c4b174a85dbd887522bb67d67f72efb
DavidTrujilloR <davidtrujilloruiz20@gmail.com>
2024-11-27 01:37:53 -0600

    ultimos cambios de proyecto

--- routes/editUser.js
+++ routes/editUser.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
+
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   const query = 'SELECT * FROM users WHERE id = ?';
@@ -11,6 +12,21 @@ router.get("/:id", (req, res) => {
       res.send('Error');
     } else {
       res.render('edit', { user: results[0] });
+    }
+  });
+});
+
+
+router.post("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  const query = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
+  db.query(query, [name, email, id], (err) => {
+    if (err) {
+      console.error('Error updating user:', err);
+      res.send('Error');
+    } else {
+      res.redirect('/');
     }
   });
 });
